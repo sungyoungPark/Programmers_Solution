7f52caabb1b27333da7544109d72576e65ed2fa5
sungyoungPark <sygitter1491012@gmail.com>
2020-03-23 01:55:02 +0900

    Initial Commit

---
+++ Programmers_Solution/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Programmers_Solution
+//
+//  Created by 박성영 on 23/03/2020.
+//  Copyright © 2020 박성영. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
