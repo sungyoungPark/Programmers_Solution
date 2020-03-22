1c2d88f3b0674540d9e65b2087b3e94715ab79b1
sungyoungPark <sygitter1491012@gmail.com>
2020-03-23 01:58:13 +0900

    https://programmers.co.kr/learn/courses/30/lessons/60058

--- Programmers_Solution/main.swift
+++ Programmers_Solution/main.swift
@@ -1,12 +1,91 @@
-//
-//  main.swift
-//  Programmers_Solution
-//
-//  Created by 박성영 on 23/03/2020.
-//  Copyright © 2020 박성영. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+var strBracket = ""
+
+
+func solution(_ p:String) -> String {
+    strBracket = p
+   
+    return checkBracket(p)
+}
+
+func checkBracket(_ p : String) -> String{
+    strBracket = p
+    var bracketCount = 0
+    
+    var u = ""
+    var flag = 0
+    var newU = ""
+    
+    for i in strBracket{
+        if i == "("{
+            bracketCount -= 1
+        }
+        else{
+            bracketCount += 1
+        }
+        u.append(i)
+        strBracket.removeFirst()
+        if bracketCount == 0{
+            break
+        }
+    }
+    
+    if u.first == "("{
+        flag = 1
+    }
+    
+//    print(u)
+//    print(strBracket)
+//    print("---")
+    if flag == 0{ //올바르지 않은 괄호 문자열 u
+        //print("올바르지 않음")
+        //newU.append("(")
+        if strBracket != ""{
+        newU = "("+checkBracket(strBracket)+")"
+        }
+        else if strBracket == "" {
+            newU.insert("(", at: newU.startIndex)
+            newU.append(")")
+        }
+        if u != ""{
+                  u.removeFirst()
+                  u.removeLast()
+                  for i in u{
+                      if i == "("{
+                          newU.append(")")
+                      }
+                      else{
+                          newU.append("(")
+                      }
+                  }
+              }
+        
+    }
+    else if flag == 1 { //올바른 괄호 문자열 u
+        //print("올바름")
+        if strBracket != ""{
+            newU = u + checkBracket(strBracket)
+           // print(u)
+        }
+        else if strBracket == ""{
+            newU = u
+        }
+        
+        
+    }
+    
+    //print(newU)
+    return newU
+    //print(strBracket)
+}
+
+//solution("()))((()")
+
+
+print(solution("(()())()"))
+print("——")
+print(solution(")("))
+print("——")
+print(solution("()))((()"))
 
