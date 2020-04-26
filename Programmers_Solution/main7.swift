43eed80dca45d44f80cb7208643a8189ed2db340
sungyoungPark <55025822+sungyoungPark@users.noreply.github.com>
2020-04-27 05:21:59 +0900

    https://programmers.co.kr/learn/courses/30/lessons/64065  2019 카카오 개발자 겨울 인턴십 > 튜플

---
+++ Programmers_Solution/main7.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func solution(_ s:String) -> [Int] {
+    var result = [Int]()
+    var val = s
+    val.removeFirst()
+    val.removeLast()
+    let arr = val.components(separatedBy: "},")
+    var myDic : [Int:[Int]] = [:]
+    for i in arr{
+        var transfer = i
+        transfer = transfer.replacingOccurrences(of: "}", with: "")
+        transfer = transfer.replacingOccurrences(of: "{", with: "")
+        transfer = transfer.replacingOccurrences(of: ",", with: " ")
+        let arr = transfer.components(separatedBy: " ")
+        var dicVal : [Int] = []
+        for t in arr{
+            dicVal.append(Int(String(t))!)
+        }
+        myDic.updateValue(dicVal, forKey: arr.count)
+    }
+    if arr.count > 1{
+        for i in 1...arr.count{
+            let dic = myDic[i]
+            for d in dic!{
+                if result.contains(d) == false{
+                    result.append(d)
+                }
+            }
+            
+        }
+    }
+    else{
+        return myDic.first!.value
+    }
+    return result
+}
+
+
+print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
+print(solution("{{123}}"))
