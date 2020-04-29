b923c5b9a38fbfbe99897a67d154ec006b5f159e
sungyoungPark <55025822+sungyoungPark@users.noreply.github.com>
2020-04-29 18:07:49 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12914  연습문제 > 멀리 뛰기

---
+++ Programmers_Solution/main10.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+//문제가 피보나치 수열 원리인 것을 알아야함
+
+func solution(_ n:Int) -> Int {
+    var arr = [1,2]
+    if n < 3{
+        return arr[n-1] % 1234567
+    }
+    else{
+        for _ in 3...n{
+            let temp = arr[1]
+            arr[1] = (arr[0] + arr[1]) % 1234567
+            arr[0] = temp
+        }
+    }
+    return arr.last!
+}
+
+
+print(solution(4))
