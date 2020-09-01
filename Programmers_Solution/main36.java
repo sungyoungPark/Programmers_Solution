e707fa6f8d42fb5637e7fd988fa8088fc1491ae0
sungyoungPark <55025822+sungyoungPark@users.noreply.github.com>
2020-09-01 18:40:54 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12927  연습문제 > 야근 지수

---
+++ Programmers_Solution/main36.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i= 0;i<works.length;i++){
+            queue.offer(works[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            int k = queue.poll();
+            if(k== 0){
+                return 0;
+            }
+            k--;
+            //System.out.print(k+ " ");
+            queue.offer(k);
+        }
+
+        while (queue.isEmpty() ==false){
+            answer = answer + (int)Math.pow(queue.poll(),2);
+        }
+        return answer;
+    }
+
+
+    public static void main(String args[]) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // String[] str = br.readLine().split(" ");
+        int[] arr = {4,3,3};
+        int[] arr2 = {2, 1, 2};
+        int[] arr3 = {1, 1};
+        System.out.println(solution(4,arr));
+        System.out.println(solution(1,arr2));
+        System.out.println(solution(3,arr3));
+    }
+
+
+}
