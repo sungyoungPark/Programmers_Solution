526714d86fe5736c3edb9c14aad89557b1cbe5b9
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2022-11-04 20:21:40 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/133502 연습문제 > 햄버거 만들기

---
+++ Programmers_Solution/main42.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ food:[Int]) -> String {
+    let onlyFood = Array(food[1...])
+    var a = 1
+    let des = onlyFood.map { (number: Int) -> String in
+        var res = ""
+        
+        if number > 1 {
+            for _ in 0...number / 2 - 1{
+                res += String(a)
+            }
+        }
+        
+        a += 1
+        return res
+    }.joined()
+   
+    return des + "0" + des.reversed()
+}
+
+
+solution([1, 3, 4, 6])
+solution([1, 7, 1, 2])
