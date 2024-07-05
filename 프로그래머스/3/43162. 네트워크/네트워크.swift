b2368fde262312622d468b5fe462c21411dd1494
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2024-07-06 04:06:24 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+var network = 0
+var graph = [[Int]]()
+var visitd = [Bool]()
+
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+    
+    graph = [[Int]](repeating: [], count: n)
+    visitd = [Bool](repeating: false, count: n)
+    
+    for i in 0...n-1{
+        for j in 0...n-1{
+            if i != j {
+                if computers[i][j] == 1{
+                    graph[i] = graph[i] + [j+1]
+                }
+            }
+        }
+    }
+    
+    dfsAll(n)
+   // print(graph)
+    //print(visitd)
+    return network
+}
+
+func dfs(_ point : Int){
+     visitd[point] = true
+    if graph[point] == [] {
+        return
+    }
+ 
+    for i in graph[point]{
+        if visitd[i-1] == false{
+            dfs(i-1)
+        }
+    }
+    
+    
+}
+
+func dfsAll(_ n :Int){
+    for i in 0...n-1{
+        if visitd[i] == false{
+            dfs(i)
+            network+=1
+        }
+    }
+}
+
+
