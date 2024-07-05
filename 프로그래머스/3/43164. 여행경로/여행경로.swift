f1e4eb02589c62799e97aeda7f53f4d4bbb8ab60
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2024-07-06 02:23:52 +0900

    [level 3] Title: 여행경로, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+var ans = [String]()
+var dic = [String:[String]]()
+var count = 0
+
+func solution(_ tickets:[[String]]) -> [String] {
+    count = tickets.count + 1
+    for ticket in tickets{
+        var arr = dic[ticket[0]]
+        if arr == nil {
+            dic.updateValue([ticket[1]], forKey: ticket[0])
+        }
+        else{
+            arr?.append(ticket[1])
+            arr?.sort()
+            dic.updateValue(arr!, forKey: ticket[0])
+        }
+        //        print(arr)
+    }
+    //ans.append("ICN")
+    dfs("ICN" )
+    //ans.append(last)
+    //print(last)
+    return ans
+}
+
+
+func dfs(_ start : String){
+    
+    let dest = dic[start]
+    ans.append(start)
+    if dest != nil{
+        for i in dest!{
+            dic[start]?.removeFirst()
+            dfs(i)
+            if ans.count == count{
+                //last = i
+                return
+            }
+            ans.removeLast()
+            dic[start]?.append(i)
+        }
+    }
+   
+}
