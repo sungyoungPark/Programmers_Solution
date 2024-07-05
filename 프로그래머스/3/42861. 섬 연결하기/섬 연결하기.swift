32b75b27eb4d73bf83ccdce509dd39f128469820
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2024-07-06 03:44:05 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+    var graph = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
+    var visit = [Bool](repeating: false, count: n)
+
+    for cost in costs{
+        graph[cost[0]][cost[1]] = cost[2]
+        graph[cost[1]][cost[0]] = cost[2]
+    }
+    
+    visit[0] = true
+    var list = [0]
+    var sum = 0
+    while true {
+        if(list.count >= n){
+            break
+        }
+        var min = Int.max
+        var min_index = 0
+        for point in list{
+            for j in 0...n-1{
+                if(visit[j] == false && graph[point][j] != 0){
+                    if(min > graph[point][j]){
+                        min = graph[point][j]
+                        min_index = j
+                    }
+                }
+            }
+        }
+        visit[min_index] = true
+        list.append(min_index)
+        sum += min
+    }
+    
+    
+    
+    return sum
+}
