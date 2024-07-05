79ba380773fd45c0ea0648905afecf05227b83ae
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2024-07-06 03:43:16 +0900

    [level 3] Title: 베스트앨범, Time: 0.17 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+var dic = [String:Int]()
+var dic2 = [String:[[Int]]]()
+
+func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
+    var ans = [Int]()
+    for i in 0...genres.count-1{
+        if dic[genres[i]] == nil{
+            dic.updateValue(plays[i], forKey: genres[i])
+        }
+        else{
+            let cur = dic[genres[i]]
+            dic.updateValue(cur! + plays[i], forKey: genres[i])
+        }
+        
+        if dic2[genres[i]] == nil{
+            dic2.updateValue([[plays[i],i]], forKey: genres[i])
+        }
+        else{
+            var cur = dic2[genres[i]]
+            let update = [plays[i],i]
+            cur?.append(update)
+            cur = cur?.sorted(by: { $0[0] > $1[0]})
+            dic2.updateValue(cur!, forKey: genres[i])
+        }
+        
+    }
+    //print(dic)
+    //print(dic2)
+    let sortDic = dic.sorted(by: {$0.value > $1.value})
+    for i in sortDic{
+        //print(i)
+        let update = dic2[i.key]
+        if update!.count >= 2{
+            for k in 0...1{
+                ans.append(update![k][1])
+            }
+        }
+        else{
+            ans.append(update![0][1])
+        }
+        
+    }
+    
+    
+    return ans
+}
