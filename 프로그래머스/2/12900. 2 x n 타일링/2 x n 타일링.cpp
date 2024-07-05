0c243cab17deabd4bc76fbaa7db9c59c32dfaff4
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2024-07-06 04:13:30 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.17 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int tmp1 = 1;
+    int tmp2 = 2;
+    int tmp3 = 0;
+    if(n==2){
+        return tmp2;
+    }
+    if(n==1){
+        return tmp1;
+    }
+    
+    for(int i=0;i<n-2;i++){
+        tmp3 = (tmp1+tmp2)% 1000000007;
+        tmp1 = tmp2;
+        tmp2 = tmp3;
+    }
+    
+    
+    return tmp3 ;
+}
