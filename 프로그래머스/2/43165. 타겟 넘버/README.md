3c1ed4d1ebb7c539152dc18fba7516f660df7d25
박성영 <55025822+sungyoungPark@users.noreply.github.com>
2025-11-26 21:42:46 +0900

    [level 2] Title: 타겟 넘버, Time: 38.88 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 25.88 ms
+메모리: 16.4 MB, 시간: 38.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 06일 02:03:37
+2025년 11월 26일 21:02:50
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,53 +1,23 @@
 import Foundation
 
-let n = 2
-var r = 0
-var rePerArr1 = [Int]()
-var result = 0
-var myNum = [Int]()
-//중복순열 (순서있게 배열 + 자시자신도 포함)
-
-
-func rePermutation(_ n : Int , _ r : Int , _ rePerArr : [Int], _ target : Int){
-    if rePerArr.count == r {
-        var count = 0
-        var tmp = 0
-        for i in rePerArr{
-            
-            if i == 0 {
-                tmp = tmp + myNum[myNum.index(myNum.startIndex, offsetBy: count)] * -1
-               // print( -1, terminator : " ")
-            }
-            else{
-                tmp = tmp + myNum[myNum.index(myNum.startIndex, offsetBy: count)] * i
-               // print(i, terminator: " ")
-            }
-            count += 1
-        }
-        if tmp == target{
-            //print("target 확인")
-            result += 1
-        }
-        
-        //print()
-        return
-    }
-    
-    for i in 0...n-1 {
-        rePerArr1.append(i)
-        rePermutation(n, r, rePerArr1, target)
-        rePerArr1.removeLast()
-    }
-    
-}
-
-
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    myNum = numbers
-    r = numbers.count
-   // print("***중복순열(0,1,2)***")
-
-    rePermutation(n, r, rePerArr1, target)
+    var res = 0
     
-    return result
+    func dfs(_ index : Int, _ sum : Int) {
+     
+        if index == numbers.count {
+            
+            if sum == target {
+                res += 1
+            }
+            return
+        }
+        dfs(index + 1, sum + numbers[index])
+        dfs(index + 1, sum - numbers[index])
+        
+    }
+    
+    dfs(0, 0)
+    
+    return res
 }
